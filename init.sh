5a8c3ac08262d2379b420b83e37eb282e770394e
Evgeniy <eugenxtk@gmail.com>
2024-03-18 21:54:22 +0300

    .

--- init.sh
+++ init.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# 1. Install Nix and packages
 NIX_PROFILE=~/.local/state/nix/profiles/profile
 NIX_PROFILE_SYMLINK=~/.nix-profile 
 
 if ! [[ -L "$NIX_PROFILE_SYMLINK" ]]; then 
-	sh <(curl -L https://nixos.org/nix/install) --daemon
+	echo "Download Nix and packages..."
+	source <(curl -L https://nixos.org/nix/install) --daemon
+	# Delete Nix profile symlink manually if you want to reinstall it
 	if ! [[ -L $NIX_PROFILE_SYMLINK ]]; then
+		echo "Creating Nix profile symlink..."
 		ln -s $NIX_PROFILE $NIX_PROFILE_SYMLINK	
 	fi
-	. $NIX_PROFILE/etc/profile.d/nix.sh 
+	. $NIX_PROFILE/etc/profile.d/nix.sh
 	nix-env -iA \ 
 		nixpkgs.zsh \
 		nixpkgs.git \
@@ -23,26 +25,27 @@ if ! [[ -L "$NIX_PROFILE_SYMLINK" ]]; then
 		nixpkgs.tree
 fi
 
-# 2. Sync with remote repository and 
 DOTFILES=~/.dotfiles
 DOTFILES_REMOTE=git@github.com:eugenxtk/.dotfiles.git
 
 if ! [[ -d $DOTFILES ]]; then
-	mkdir $DOTFILES 
+	echo "Cloning remote repository..."
 	git clone $DOTFILES_REMOTE $DOTFILES
 fi
 
-# 3. Set up ZSH as default shell
 ZSHRC=$DOTFILES/.zshrc
 ZSHRC_SYMLINK=~/.zshrc
 
+if ! [[ -L $ZSHRC_SYMLINK ]]; then
+	echo "Creating ZSH symlink..."
+	ln -s $ZSHRC $ZSHRC_SYMLINK
+fi
+
 if ! [[ $SHELL == *"zsh"* ]]; then
+	echo "Setting up ZSH as default shell..."
 	command -v zsh | sudo tee -a /etc/shells
  	sudo chsh -s $(which zsh)
- 	echo "Now ZSH is your default shell. You need to restart session to continue."
+ 	echo "Now ZSH is default shell. You need to restart session to continue."
  	exit 1
 fi
 	
-if ! [[ -L $ZSHRC_SYMLINK ]]; then
-	ln -s $ZSHRC $ZSHRC_SYMLINK
-fi
