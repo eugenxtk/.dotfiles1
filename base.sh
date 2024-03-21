50a215f248052e7860c1a4e644de4c4a1b5d8436
Eugen Tkach <55444010+eugenxtk@users.noreply.github.com>
2024-03-22 00:06:16 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
-NIX_PROFILE=~/.nix-profile 
+DOTFILES=~/.dotfiles
 
-if ! [[ -L "$NIX_PROFILE" ]]; then 
-	echo "Download Nix and packages..."
-	sh <(curl -L https://nixos.org/nix/install) --no-daemon
-	. $NIX_PROFILE/etc/profile.d/nix.sh
-	nix-env -iA \ 
-		nixpkgs.zsh \
-		nixpkgs.git \
-		nixpkgs.neovim \
-		nixpkgs.tmux \
-		nixpkgs.stow \
-		nixpkgs.fzf \
-		nixpkgs.fd \
-		nixpkgs.ripgrep \
-		nixpkgs.bat \
-		nixpkgs.tree
+NIX_PROFILE=~/.nix-profile
+
+if ! [[ -L "$NIX_PROFILE" ]]; then
+        echo "Download Nix and packages..."
+        sh <(curl -L https://nixos.org/nix/install) --no-daemon
 fi
 
-DOTFILES=~/.dotfiles
-DOTFILES_REMOTE=https://github.com/eugenxtk/.dotfiles.git
-
-if ! [[ -d $DOTFILES ]]; then
-	echo "Cloning remote repository..."
-	git clone $DOTFILES_REMOTE $DOTFILES
+if ! [[ $(command -v zsh) == *zsh* ]]; then
+        echo "Installing ZSH..."
+        sudo apt install zsh
 fi
 
 ZSHRC=$DOTFILES/.zshrc
+ZSHRC_REMOTE=https://raw.githubusercontent.com/eugenxtk/.dotfiles/main/.zshrc
+
 ZSHRC_SYMLINK=~/.zshrc
 
-if ! [[ -L $ZSHRC_SYMLINK ]]; then
-	echo "Creating ZSH symlink..."
-	ln -s $ZSHRC $ZSHRC_SYMLINK
+if ! [[ -e $ZSHRC ]]; then
+        echo "Getting ZSHRC from remote repository..."
+        curl -L $ZSHRC_REMOTE > $ZSHRC
+
+        if [[ -e $ZSHRC_SYMLINK ]]; then
+                rm $ZSHRC_SYMLINK
+        fi
+        ln -s $ZSHRC $ZSHRC_SYMLINK
 fi
 
-if ! [[ $SHELL == *"zsh"* ]]; then
-	echo "Setting up ZSH as default shell..."
- 	command -v zsh | sudo tee -a /etc/shells
-	sudo chsh -s "$(command -v zsh)" "${USER}"
- 	exit 1
+ANTIGEN=~/antigen.zsh
+ANTIGEN_REMOTE=git.io/antigen
+
+if ! [[ -e $ANTIGEN ]]; then
+        echo "Installing Antigen..."
+        curl -L $ANTIGEN_REMOTE > $ANTIGEN
 fi
-	
+
+if ! [[ $SHELL == *zsh* ]]; then
+        echo "Setting up ZSH as default shell..."
+        command -v zsh | sudo tee -a /etc/shells
+        sudo chsh -s "$(command -v zsh)" "${USER}"
+        exit 1
+fi
