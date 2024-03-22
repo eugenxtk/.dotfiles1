a8e53d36796a87f9e332b82bfa2f70735324f71f
Eugen Tkach <55444010+eugenxtk@users.noreply.github.com>
2024-03-23 00:45:58 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -32,5 +32,5 @@ if ! [[ $SHELL == *zsh* ]]; then
         echo "Setting up ZSH as default shell..."
         sudo sh -c "echo $(which zsh) >> /etc/shells"
         sudo chsh -s "$(command -v zsh)" "${USER}"
-        exit 1
+        exit 0
 fi
